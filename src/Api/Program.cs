145d0c55505c84a405ecfec5ef827d8036f305c1
Jonathan Robertson <jonathan.robertson@aquaticinformatics.com>
2022-10-06 16:25:41 -0600

    added authorized controller - returns 401

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// JR added
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -17,6 +32,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+// JR added
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 
 app.UseAuthorization();
 
