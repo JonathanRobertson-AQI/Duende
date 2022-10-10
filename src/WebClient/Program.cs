56e6520d6b6fea495a559bfd13df402ebf4b1bdc
Jonathan Robertson <jonathan.robertson@aquaticinformatics.com>
2022-10-10 11:29:56 -0600

    add claims

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,8 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Clear();
         options.Scope.Add("openid");
         options.Scope.Add("profile");
+        options.Scope.Add("verification");
+        options.ClaimActions.MapJsonKey("email_verified", "email_verified");
         options.GetClaimsFromUserInfoEndpoint = true;
 
         options.SaveTokens = true;
